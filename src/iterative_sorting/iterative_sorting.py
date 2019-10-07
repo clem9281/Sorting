01c1475790d1a078195a7ca12604df409184b3d6
clem9281 <clem9281@gmail.com>
2019-10-07 14:47:20 -0700

    completes count sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -35,4 +35,27 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
 
-    return arr
+    # I want to get the actual max value from the array, rather than using max I'll manually iterate through, find the max value, and give the user an error if we encounter a negative number
+    for element in arr:
+        if element < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        elif element > maximum:
+            maximum = element
+
+    # create an array of max + 1 zeroes
+    count_arr = [0] * (maximum + 1)
+
+    for element in arr:
+        count_arr[element] += 1
+
+    total = 0
+    for i in range(0, maximum + 1):
+        temp = count_arr[i]
+        count_arr[i] = total
+        total += temp
+
+    output = [None] * len(arr)
+    for element in arr:
+        output[count_arr[element]] = element
+        count_arr[element] += 1
+    return output
